5d0a9c3ad7d7cba467cd4dd538bf6b874cf45843
Extreme-Studios <3xtremestudios@gmail.com>
2026-09-11 00:03:43 +0700

    map structure portraits to Drive image names

--- assets/pengurus-drive-images.js
+++ assets/pengurus-drive-images.js
@@ -7,8 +7,14 @@
     .then(data => {
       if (!data.ok) throw new Error(data.error || 'Foto pengurus belum tersedia.');
       const byName = new Map((data.items || []).map(item => [normalize(item.name), item.url]));
+      const byNumber = new Map((data.items || []).map(item => {
+        const number = String(item.name || '').match(/^0*(\d+)_/);
+        return number ? [String(Number(number[1])), item.url] : null;
+      }).filter(Boolean));
       images.forEach(image => {
-        const remote = byName.get(normalize(image.dataset.pengurusImage));
+        const localName = image.dataset.pengurusImage;
+        const number = String(localName || '').match(/(?:all-)?pengurus-(\d+)/i);
+        const remote = byName.get(normalize(localName)) || (number ? byNumber.get(String(Number(number[1]))) : '');
         if (!remote) return;
         image.src = remote;
         image.dataset.pengurusDrive = 'true';
